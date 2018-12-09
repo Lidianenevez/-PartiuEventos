4f83c157c228fc4f48aea01244096f0de343f7de
Alex Rodrigues <alexcleiton16@gmail.com>
2018-12-08 23:24:57 -0300

    excluções

--- resources/views/admin/evento.blade.php
+++ resources/views/admin/evento.blade.php
@@ -3,21 +3,21 @@
 @section('content')
 <div class="container" style="margin-top:100px">
 	@if (session('msg_success'))
-						<div class="alert alert-success">
-							{{ session('msg_success') }}
-						</div>
-					@endif
-					@if (session('msg_danger'))
-						<div class="alert alert-danger">
-							{{ session('msg_danger') }}
-						</div>
-					@endif
+		<div class="alert alert-success">
+			{{ session('msg_success') }}
+		</div>
+	@endif
+	@if (session('msg_danger'))
+		<div class="alert alert-danger">
+			{{ session('msg_danger') }}
+		</div>
+	@endif
 	<div class="row justify-content-center">
 		<div class="col-md-8">
 			<div class="card">
 				<div class="card-header">Relatórios</div>
 				<div class="card-body">
-					dfsu
+					Visistas
 				</div>
 			</div>
 		</div>
