0a1f181cb6aef69687fa8a2536aa2d0469a05fc3
Alex Rodrigues <alexcleiton16@gmail.com>
2018-12-10 04:08:13 -0300

    update

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -14,7 +14,6 @@
   <!-- Favicons -->
   <link rel="icon" type="image/png" sizes="32x32" href="{{ url('mg/favicon-32x32.png') }}">
   <link rel="icon" type="image/png" sizes="96x96" href="{{ url('img/favicon-96x96.png') }}">
-  <link rel="manifest" href="{{ url('img/manifest.json') }}">
   <meta name="msapplication-TileColor" content="#ffffff">
   <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
   <meta name="theme-color" content="#ffffff">
