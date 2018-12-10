b556a57bddb555c5cdf89420edf171be5c9ced35
Alex Rodrigues <alexcleiton16@gmail.com>
2018-12-10 04:01:40 -0300

    update

--- app/DateTime.php
+++ app/DateTime.php
@@ -6,14 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class DateTime extends Model
 {
-    protected $table = 'datetime_eventos';
-
-    public $timestamps = false;
-    /**
-	 * Get the eventos that owns the date time.
-	 */
-	public function evento()
-	{
-		return $this->belongsTo('App\Evento', 'fk_evento_id');
-	}
+  protected $table = 'datetime_eventos';
+	public $timestamps = false;
 }
