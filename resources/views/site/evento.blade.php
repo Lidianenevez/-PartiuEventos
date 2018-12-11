613a9b80f3621730eff544cc94663904c0d09a4b
Lidiane <lidianeneves79@gmail.com>
2018-12-11 00:21:37 -0300

    avaliar por estrela

--- resources/views/site/evento.blade.php
+++ resources/views/site/evento.blade.php
@@ -96,6 +96,32 @@
             <div class="details">
               <center>
                 <h3><a class="nomeevento" href="{{ route('evento.mostrar', [$e->id,kebab_case($e->nome_evento)]) }}">{{ $e->nome_evento }}</a></h3>
+                 <div class="social-links">
+                      <form method="POST" action="processa.php" enctype="multipart/form-data">
+      <div class="estrelas">
+        <input type="radio" id="vazio" name="estrela" value="" checked>
+        
+        <label for="estrela_um"><i class="fa"></i></label>
+        <input type="radio" id="estrela_um" name="estrela" value="1">
+        
+        <label for="estrela_dois"><i class="fa"></i></label>
+        <input type="radio" id="estrela_dois" name="estrela" value="2">
+        
+        <label for="estrela_tres"><i class="fa"></i></label>
+        <input type="radio" id="estrela_tres" name="estrela" value="3">
+        
+        <label for="estrela_quatro"><i class="fa"></i></label>
+        <input type="radio" id="estrela_quatro" name="estrela" value="4">
+        
+        <label for="estrela_cinco"><i class="fa"></i></label>
+        <input type="radio" id="estrela_cinco" name="estrela" value="5">
+        
+        <input type="submit" value="Avaliar" class="btn btn-outline-warning" style="margin-bottom: 7px;">
+        
+      </div>
+    </form>
+                    </div>
+                
                 <div class="social">
                   <a  data-toggle="tooltip" data-placement="bottom" title="endereço"><i class="fa fa-map-marker" aria-hidden="true"></i></a>
                   <a  data-toggle="tooltip" data-placement="bottom" title="data-inicio"><i class="fa fa-calendar" aria-hidden="true"></i>
